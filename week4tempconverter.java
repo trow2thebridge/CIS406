aa92b124c0c7db046c2501f080e4245475e1cac7
trow2thebridge <109030674+trow2thebridge@users.noreply.github.com>
2024-08-27 14:19:37 -0500

    Update week4tempconverter.java

--- week4tempconverter.java
+++ week4tempconverter.java
@@ -1,6 +1,33 @@
 package temperatureConversion;
 
-public class TemperatureConversion
+import java.util.Scanner;
+
+public class TemperatureConversion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Welcome to the Temperature Converter");
+        
+        System.out.print("Enter degrees in Fahrenheit: ");
+        double fahrenheit = scanner.nextDouble();
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.println("Degrees in Celsius: " + celsius);
+        
+        System.out.print("Enter degrees in Celsius: ");
+        celsius = scanner.nextDouble();
+        fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Degrees in Fahrenheit: " + fahrenheit);
+        
+        System.out.println("Bye!!");
+        scanner.close();
+    }
+}
+
+
+
+
+
+
 
 
 import java.util.Scanner;

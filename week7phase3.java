5b1e520c2779e0ebc94c22a75ea9774378892f89
trow2thebridge <109030674+trow2thebridge@users.noreply.github.com>
2024-08-28 11:06:35 -0500

    Update week7phase3.java

--- week7phase3.java
+++ week7phase3.java
@@ -1,5 +1,75 @@
 package orderEntryPhase3;
 
+import java.util.Scanner;
+
+public class OrderEntryPhase3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String continueInput = "y";
+        int totalLineItems = 0;
+        double totalGrossAmount = 0.0;
+        double totalTaxAmount = 0.0;
+        double totalDiscountAmount = 0.0;
+        double totalNetAmount = 0.0;
+
+        while (continueInput.equalsIgnoreCase("y")) {
+            System.out.print("Enter item number: ");
+            String itemNumber = scanner.nextLine();
+            System.out.print("Enter item description: ");
+            String itemDescription = scanner.nextLine();
+            System.out.print("Enter the item price: ");
+            double itemPrice = scanner.nextDouble();
+            System.out.print("Enter quantity ordered: ");
+            int quantityOrdered = scanner.nextInt();
+            double grossAmount = itemPrice * quantityOrdered;
+
+            System.out.print("Enter the tax percentage: ");
+            double taxPercentage = scanner.nextDouble();
+            double taxAmount = grossAmount * (taxPercentage / 100);
+
+            System.out.print("Enter the discount percent: ");
+            double discountPercentage = scanner.nextDouble();
+            double discountAmount = grossAmount * (discountPercentage / 100);
+
+            double netAmount = grossAmount + taxAmount - discountAmount;
+
+            System.out.println("Item Number: " + itemNumber);
+            System.out.println("Item Description: " + itemDescription);
+            System.out.println("Item Price: " + String.format("%.2f", itemPrice));
+            System.out.println("Quantity Ordered: " + quantityOrdered);
+            System.out.println("Tax Percentage: " + String.format("%.2f", taxPercentage));
+            System.out.println("Tax Amount: " + String.format("%.2f", taxAmount));
+            System.out.println("Discount Percentage: " + String.format("%.2f", discountPercentage));
+            System.out.println("Discount Amount: " + String.format("%.2f", discountAmount));
+            System.out.println("Net Amount: " + String.format("%.2f", netAmount));
+            System.out.println();
+
+            totalLineItems++;
+            totalGrossAmount += grossAmount;
+            totalTaxAmount += taxAmount;
+            totalDiscountAmount += discountAmount;
+            totalNetAmount += netAmount;
+
+            System.out.print("Do you want to enter another item? (y/n): ");
+            continueInput = scanner.next();
+            scanner.nextLine(); // Consume newline
+        }
+
+        System.out.println("Total Line Items: " + totalLineItems);
+        System.out.println("Total Gross Amount: " + String.format("%.2f", totalGrossAmount));
+        System.out.println("Total Tax Amount: " + String.format("%.2f", totalTaxAmount));
+        System.out.println("Total Discount Amount: " + String.format("%.2f", totalDiscountAmount));
+        System.out.println("Total Net Amount: " + String.format("%.2f", totalNetAmount));
+
+        scanner.close();
+        System.out.println("Bye!!");
+    }
+}
+
+
+
+package orderEntryPhase3;
+
 public class OrderEntryPhase3 {
 
 

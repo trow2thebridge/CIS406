dbc06d0ffefdf61b9aa888f29175776dcdcf5973
trow2thebridge <109030674+trow2thebridge@users.noreply.github.com>
2024-08-28 10:50:10 -0500

    Update powertablew6.java

--- powertablew6.java
+++ powertablew6.java
@@ -1,3 +1,43 @@
+
+package powersTable;
+
+import java.util.Scanner;
+
+public class PowersTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String continueInput;
+
+        do {
+            System.out.print("Enter an integer: ");
+            int n = scanner.nextInt();
+
+            System.out.println("Number");
+            for (int i = 1; i <= n; i++) {
+                System.out.println(i);
+            }
+
+            System.out.println("Squared");
+            for (int i = 1; i <= n; i++) {
+                System.out.println(i * i);
+            }
+
+            System.out.println("Cubed");
+            for (int i = 1; i <= n; i++) {
+                System.out.println(i * i * i);
+            }
+
+            System.out.print("Continue? (y/n): ");
+            continueInput = scanner.next();
+        } while (continueInput.equalsIgnoreCase("y"));
+
+        System.out.println("Bye!!");
+        scanner.close();
+    }
+}
+
+
+
 import java.util.Scanner;
 
 public class SquaresAndCubes {
